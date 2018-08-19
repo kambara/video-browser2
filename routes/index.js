49e72980e0b6ea3bfc4a1c178ceb265ae0fa1a62
Keisuke Kambara <kambara@sappari.org>
2018-08-19 13:00:16 +0900

    browser title

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const config = require('config')
 
 router.get('/*', function(req, res) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: config.title })
 })
 
 module.exports = router
